d71cd6224de1968841f608f0894994d09d9c3431
anthonybishopric <anthonybishopric@gmail.com>
2012-11-08 11:05:08 -0800

    Added test for Shmockers

---
+++ test/Shmockers_Test.php
@@ -0,0 +1,48 @@
+<?php
+
+class Shmockers_Test extends PHPUnit_Framework_TestCase
+{
+	use Shmockers;
+	
+	public function test_this_test_can_use_the_shmock_helper_methods()
+	{
+		
+		$shmock_email_service = $this->shmock('Email_Service', function($user)
+		{
+			$user->send('user@gmail.com', $this->stringContains("a message"));
+		});
+		
+		$user = new User($shmock_email_service);
+		$user->set_email_address('user@gmail.com');
+		$user->send_email("This is a message");
+	}
+}
+
+class User
+{
+	private $email_address;
+	private $email_service;
+	
+	public function __construct($email_service)
+	{
+		$this->email_service = $email_service;
+	}
+	
+	public function set_email_address($email)
+	{
+		$this->email_address = $email;
+	}
+	
+	public function send_email($message)
+	{
+		$this->email_service->send($this->email_address, $message);
+	}
+}
+
+class Email_Service
+{
+	public function send($email, $message)
+	{
+		// no-op, just for testing
+	}
+}
