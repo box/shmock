eea52b21e0591161e8f6c2e024c959e16ab100db
Tim Heilig <theilig@box.com>
2017-02-28 12:18:05 -0800

    Minimize perf problem with return_value_map. Should consider eliminating differ completely, but for now only taking the hit when we are going to throw an exception anyway.

--- src/Shmock/StaticSpec.php
+++ src/Shmock/StaticSpec.php
@@ -299,17 +299,20 @@ class StaticSpec implements Spec
 
         $this->will = function ($invocation) use ($mapping) {
             $args = $invocation->parameters;
-            $differ = new \SebastianBergmann\Diff\Differ();
-            $diffSoFar = null;
             foreach ($mapping as $map) {
                 list($possibleArgs, $possibleRet) = $map;
                 if ($possibleArgs === $args) {
                     return $possibleRet;
-                } else {
-                    $nextDiff = $differ->diff(print_r($possibleArgs, true), print_r($args, true));
-                    if ($diffSoFar === null || strlen($nextDiff) < strlen($diffSoFar)) {
-                        $diffSoFar = $nextDiff;
-                    }
+                }
+            }
+
+            $differ = new \SebastianBergmann\Diff\Differ();
+            $diffSoFar = null;
+            foreach ($mapping as $map) {
+                $possibleArgs = $map[0];
+                $nextDiff = $differ->diff(print_r($possibleArgs, true), print_r($args, true));
+                if ($diffSoFar === null || strlen($nextDiff) < strlen($diffSoFar)) {
+                    $diffSoFar = $nextDiff;
                 }
             }
             throw new \PHPUnit_Framework_AssertionFailedError(sprintf("Did not expect to be called with args %s, diff with closest match is\n%s", print_r($args, true), $diffSoFar));
